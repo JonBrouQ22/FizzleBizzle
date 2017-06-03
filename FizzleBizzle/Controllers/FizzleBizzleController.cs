d5b534bcf79b53f27b0daad30f495e8c88feda5c
JonBrouQ22 <jonbrou@yahoo.com>
2017-06-03 14:13:04 -0500

    Adding first draft of application
    Created this small fizz buzz application to test my .NET MVC skills.
    Still needs some work.

---
+++ FizzleBizzle/Controllers/FizzleBizzleController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FizzleBizzle.Controllers
+{
+    public class FizzleBizzleController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new FizzleBizzleModel());
+        }
+        
+
+        [HttpPost]
+        public ActionResult FizzBuzzBazz(int start, int end, int fizz, int buzz, int? bazz, string type)
+        {
+            FizzleBizzleModel FizzBizz = new FizzleBizzleModel();
+            FizzBizz.Fizz = fizz;
+            FizzBizz.Buzz = buzz;
+            if (ModelState.IsValid)
+            {
+                if (bazz != null)
+                {
+                    Predicate<int> pred = (x => x == 0);
+                    switch (type)
+                    {
+                        case "gt":
+                            pred = (x => x > bazz);
+                            break;
+                        case "lt":
+                            pred = (x => x < bazz);
+                            break;
+                        case "eq":
+                            pred = (x => x == bazz);
+                            break;
+                    }
+                    ViewBag.Result = FizzBizz.FizzBuzzBazz(start, end, pred);
+                }
+                else
+                {
+                    ViewBag.Result = FizzBizz.FizzBuzz(start, end);
+                }
+            }
+            return PartialView("Results");
+        }
+    }
+}
---
+++ FizzleBizzle/Models/FizzleBizzleModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FizzleBizzle
+{
+    public interface IFizzleBizzle
+    {
+        int Fizz { set; get; }
+        int Buzz { set; get; }
+
+        string[] FizzBuzz(int start, int end);
+
+        string[] FizzBuzzBazz(int start, int end, Predicate<int> bazz);
+    }
+
+    public class FizzleBizzleModel : IFizzleBizzle
+    {
+        private int _fizz;
+        private int _buzz;
+
+        [Required]
+        public int Fizz
+        {
+            get { return _fizz; }
+            set { _fizz = value; }
+        }
+
+        [Required]
+        public int Buzz
+        {
+            get { return _buzz; }
+            set { _buzz = value; }
+        }
+        public string[] FizzBuzz(int start, int end)
+        {
+            List<string> numList = new List<string>();
+
+            for(int i = start; i <= end; i++)
+            {
+                if(i % Fizz == 0 && i % Buzz == 0)
+                {
+                    numList.Add("FizzBuzz");
+                } else if(i % Fizz == 0)
+                {
+                    numList.Add("Fizz");
+                } else if(i % Buzz == 0)
+                {
+                    numList.Add("Buzz");
+                } else
+                {
+                    numList.Add(i.ToString());
+                }
+            }
+            return numList.ToArray();
+        }
+
+        public string[] FizzBuzzBazz(int start, int end, Predicate<int> bazz)
+        {
+            List<string> numList = new List<string>();
+
+            for (int i = start; i <= end; i++)
+            {
+                var boolean = bazz(i); 
+                if (i % Fizz == 0 && i % Buzz == 0 && boolean)
+                {
+                    numList.Add("FizzBuzzBazz");
+                }
+                else if(i % Fizz == 0 && i % Buzz == 0) {
+                    numList.Add("FizzBuzz");
+                }
+                else if (i % Fizz == 0)
+                {
+                    numList.Add("Fizz");
+                }
+                else if (i % Buzz == 0)
+                {
+                    numList.Add("Buzz");
+                }
+                else
+                {
+                    numList.Add(i.ToString());
+                }
+            }
+            string[] list = numList.ToArray();
+            return list;
+        }
+    }
+}
